64a776a71da20b43d08e8bfb00313a8e452023c9
Pritam Kumar <pritam176@users.noreply.github.com>
2020-08-04 20:59:57 +0400

    Update UserController.java

--- springboot-k8s-mysql/src/main/java/com/example/k8s/springbootk8smysql/controller/UserController.java
+++ springboot-k8s-mysql/src/main/java/com/example/k8s/springbootk8smysql/controller/UserController.java
@@ -17,35 +17,15 @@ import com.example.k8s.springbootk8smysql.repository.UserRepository;
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+   
 	
-	@PostMapping("/addUser")
-	public String saveUser(@RequestBody User emp) {
-		userRepository.save(emp);
-		return "User added successfully::"+emp.getId();
-		
-	}
+	
 	@GetMapping("/users")
 	public String getUsers() {
 		"Iam online";
 	}
 	
-	@GetMapping("/users")
-	public List<User> getUsers() {
-		return userRepository.findAll();
-	}
-
-	@GetMapping("/findUser/{id}")
-	public Optional<User> getUser(@PathVariable Long id) {
-		return userRepository.findById(id);
-	}
 	
-	@GetMapping("/deleteUser/{id}")
-	public String deleteUser(@PathVariable Long id) {
-		userRepository.deleteById(id);
-		return "Deleted User Successfully::"+id;
-	}
 	
 
 }
