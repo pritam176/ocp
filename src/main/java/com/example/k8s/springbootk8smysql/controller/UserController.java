bc9e3cdc173c49ce6e2747fb96bdc280788a073b
Pritam Mohapatra <pritamkumar.mohapatra@adib.com>
2023-07-23 04:05:30 +0400

    moved the structure.

--- src/main/java/com/example/k8s/springbootk8smysql/controller/UserController.java
+++ src/main/java/com/example/k8s/springbootk8smysql/controller/UserController.java
@@ -1,18 +1,13 @@
 package com.example.k8s.springbootk8smysql.controller;
 
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Slf4j
 public class UserController {
 
    
@@ -20,6 +15,7 @@ public class UserController {
 	
 	@GetMapping("/users")
 	public String getUsers() {
+		log.info("invoked");
 		return "Iam online";
 	}
 	
