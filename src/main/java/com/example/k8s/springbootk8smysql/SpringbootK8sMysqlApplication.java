744a9405c889c285244a1509d6c9bb8355169b8a
Pritam Mohapatra <pritamkumar.mohapatra@adib.com>
2023-07-24 09:13:30 +0400

    Service Mesh Test

--- src/main/java/com/example/k8s/springbootk8smysql/SpringbootK8sMysqlApplication.java
+++ src/main/java/com/example/k8s/springbootk8smysql/SpringbootK8sMysqlApplication.java
@@ -2,12 +2,21 @@ package com.example.k8s.springbootk8smysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Configuration
 public class SpringbootK8sMysqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootK8sMysqlApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getResttemplate(){
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/com/example/k8s/springbootk8smysql/controller/UserController.java
+++ src/main/java/com/example/k8s/springbootk8smysql/controller/UserController.java
@@ -6,13 +6,21 @@ import com.example.k8s.springbootk8smysql.repository.UserRepository;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
 @Slf4j
 public class UserController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${service-mesh}")
+	private String url ;
 
 
 	@Autowired
@@ -30,6 +38,13 @@ public class UserController {
 		log.info("invoked");
 		List<User> users= userRepository.findAll();
 		return "No of User + "+users.size();
+	}
+
+	@GetMapping("/rest")
+	public String getRestUsers() {
+		log.info("Rest");
+
+		return restTemplate.getForObject(url,String.class);
 	}
 	
 	
